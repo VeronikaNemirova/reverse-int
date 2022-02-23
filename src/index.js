5267718c128d6add16749a41914401230242f8e1
VeronikaNemirova <nvn92@tut.by>
2022-02-23 21:39:35 +0300

    feat: created reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const pos = Math.abs(n);
+
+  const str = pos.toString();
+
+  return str.split('').reverse().filter((item, index) => !index ? Number(item) : true).join('');
 }
